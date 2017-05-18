34890c29f3d287ede68cd8688a8311ec3e7f33c5
jeff <jeffgo0908@gmail.com>
2017-05-19 01:16:00 +0800

    add new operation example

--- src/livequery/completeNotActiveQuery.js
+++ src/livequery/completeNotActiveQuery.js
@@ -1,20 +1,18 @@
-//import { rxQuerySimple } from '../../../redux-livequery';
-import { rxQuerySimple } from 'redux-livequery';
+//import { rxQueryLeftOuterJoin } from '../../../redux-livequery';
+import { rxQueryLeftOuterJoin } from 'redux-livequery';
 export default function completeNotActiveQuery(store) {
 	console.log("completeNotActiveQuery()");
 	let selector0 = (state) => state.task.isComplete;
 	let selector1 = (state) => state.task.isActive;
-	rxQuerySimple([selector0, selector1], ['isComplete', 'isActive'], (completeActive) => {
+	rxQueryLeftOuterJoin([selector0, selector1], ['isComplete', 'isActive'], (completeNotActive) => {
 		// equal SQL =>
 		// select * from isComplete OUTER JOIN isActive on isComplete.child_key == isActive.child_key
-		console.log("got latest completeActive", completeActive);
+		console.log("got latest completeNotActive", completeNotActive);
 		// you can do whatever you want here
 		// ex: filter, reduce, map
 		let isCompleteNotActive = {};
-		for (let key in completeActive.isComplete) {
-			if (!(key in completeActive.isActive)) {
-				isCompleteNotActive[key] = completeActive.isComplete[key];
-			}
+		for (let value of completeNotActive) {
+			isCompleteNotActive[value.key] = value.isComplete;
 		}
 
 		// set data into redux state
