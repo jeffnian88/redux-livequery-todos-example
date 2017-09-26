c33edbd97bfdc5487110d131d44fe7707e440fc1
jeff <jeffgo0908@gmail.com>
2017-09-26 14:29:07 +0800

    refine code

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,3 +1,4 @@
+import { omit } from 'lodash';
 export default function taskReducer(state = { taskList: [], completeSet: {}, activeSet: {} }, action = {}) {
 	switch (action.type) {
 		case "ADD_NEW_TASK": {
@@ -17,45 +18,39 @@ export default function taskReducer(state = { taskList: [], completeSet: {}, act
 		}
 		case "MARK_TASK_COMPLETE": {
 			const { id } = action.meta;
-			state = {
+			const completeSet = { ...state.completeSet, [id]: { completed: Date.now() } };
+			return {
 				...state,
-				completeSet: { ...state.completeSet, [id]: { completed: Date.now() } }
+				completeSet,
+				completeOrActiveSet: { ...completeSet, ...state.activeSet }
 			};
-			return { ...state, completeOrActiveSet: { ...state.completeSet, ...state.activeSet } };
-		}
-		case "UNMARK_TASK_COMPLETE": {
-			const { id } = action.meta;
-			state = {
-				...state,
-				completeSet: Object.keys(state.completeSet).reduce((nextSet, key) => {
-					if (key !== id) {
-						nextSet[key] = state.completeSet[key];
-					}
-					return nextSet;
-				}, {})
-			};
-			return { ...state, completeOrActiveSet: { ...state.completeSet, ...state.activeSet } };
 		}
 		case "MARK_TASK_ACTIVE": {
 			const { id } = action.meta;
-			state = {
+			const activeSet = { ...state.activeSet, [id]: { active: Date.now() } };
+			return {
 				...state,
-				activeSet: { ...state.activeSet, [id]: { active: Date.now() } }
+				activeSet,
+				completeOrActiveSet: { ...state.completeSet, ...activeSet }
 			};
-			return { ...state, completeOrActiveSet: { ...state.completeSet, ...state.activeSet } };
+		}
+		case "UNMARK_TASK_COMPLETE": {
+			const { id } = action.meta;
+			const completeSet = omit(state.completeSet, id);
+			return {
+				...state,
+				completeSet,
+				completeOrActiveSet: { ...completeSet, ...state.activeSet }
+			};
 		}
 		case "UNMARK_TASK_ACTIVE": {
 			const { id } = action.meta;
-			state = {
+			const activeSet = omit(state.activeSet, id);
+			return {
 				...state,
-				activeSet: Object.keys(state.activeSet).reduce((result, key) => {
-					if (key !== id) {
-						result[key] = state.activeSet[key];
-					}
-					return result;
-				}, {})
+				activeSet,
+				completeOrActiveSet: { ...activeSet, ...state.completeSet }
 			};
-			return { ...state, completeOrActiveSet: { ...state.completeSet, ...state.activeSet } };
 		}
 		default:
 			return state;
--- src/store.js
+++ src/store.js
@@ -4,21 +4,11 @@ import { createLogger } from 'redux-logger';
 import promiseMiddleware from 'redux-promise';
 
 import rootReducer from './reducers';
-
-
 let appliedMiddleware;
 
 let isDEV = (typeof __DEV__ !== 'undefined') || (process.env.NODE_ENV !== 'production');
 if (isDEV) {
   appliedMiddleware = applyMiddleware(promiseMiddleware, thunk, createLogger());
-
-  // if (module.hot) {
-  //   // Enable Webpack hot module replacement for reducers
-  //   module.hot.accept('./reducers', () => {
-  //     const nextReducer = require('./reducers').default; // eslint-disable-line global-require
-  //     store.replaceReducer(nextReducer);
-  //   });
-  // }
 } else {
   appliedMiddleware = applyMiddleware(promiseMiddleware, thunk);//applyMiddleware(thunk)(createStore);
 }
@@ -30,4 +20,5 @@ const enhancer = compose(
   appliedMiddleware,
   window.devToolsExtension ? window.devToolsExtension() : f => f // add support for Redux dev tools,
 );
+
 export const store = createStore(rootReducer, { task: { taskList: [], completeSet: {}, activeSet: {} } }, enhancer);
